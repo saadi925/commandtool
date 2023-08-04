b67a3ee44bfb5a1a0df4200b6f993fc0154ca135
saadi925 <userid925925@gmail.com>
2023-08-03 18:24:27 -0700

    created build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://saadi925.github.io/git-commands-tool",
+  base: "/git-commands-tool/",
   plugins: [react()],
 });
